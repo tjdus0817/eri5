1b600343ee9da62fb4a2f28cb1f68540485bde29
tjdus0817 <100830306+tjdus0817@users.noreply.github.com>
2024-05-28 19:53:48 +0900

    Add files via upload

---
+++ Android Project/regiester.php
@@ -0,0 +1,21 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "비밀번호", "user");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $userID = $_POST["userID"];
+    $userPassword = $_POST["userPassword"];
+    $userName = $_POST["userName"];
+    $userAge = $_POST["userAge"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO user VALUES (?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "sssi", $userID, $userPassword, $userName, $userAge);
+    mysqli_stmt_execute($statement);
+
+
+    $response = array();
+    $response["success"] = true;
+
+
+    echo json_encode($response);
+
+?>
